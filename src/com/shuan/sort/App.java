8a87d065e1ef0f5d23afa510a81bca6609c250a3
ljinshuan <ljinshuan@gmail.com>
2014-04-21 18:28:52 +0800

    冒泡排序

--- src/com/shuan/sort/App.java
+++ src/com/shuan/sort/App.java
@@ -35,11 +35,33 @@ public class App {
 		}
 		System.out.println();
 	}
+	
+	/**
+	 * 冒泡排序
+	 * @param data
+	 */
+	public static int [] BubbleSort(int [] data){
+		int temp=0;
+		for (int i = 0; i < data.length; i++) {
+			//后面的数已经有序 不用比较了
+			for (int j = 0; j < data.length-i-1; j++) {
+				//如果前面一个数大于后面的数 则交换
+				if(data[j]>data[j+1]){
+					temp=data[j];
+					data[j]=data[j+1];
+					data[j+1]=temp;
+				}
+			}
+		}
+		return data;
+	} 
 	/**
 	 * @param args
 	 */
 	public static void main(String[] args) {
 		int [] data=InitRandomArray(20);
+		PrintArray(data);
+		BubbleSort(data);
 		PrintArray(data);
 	}
 
