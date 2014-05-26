46a6f48c850bdf687aba49531a823fa06f7e1d51
ljinshuan <ljinshuan@gmail.com>
2014-05-26 18:53:48 +0800

    红黑树

--- src/com/shuan/problem/RedBlackTree.java
+++ src/com/shuan/problem/RedBlackTree.java
@@ -10,26 +10,8 @@ import java.util.TreeMap;
  */
 public class RedBlackTree {
 
-	private TreeNode root;
-
-	/**
-	 * 新增一个节点
-	 * 
-	 * @param key
-	 */
-	public void add(int key) {
-		if (root == null) {
-			// 根节点
-			root = new TreeNode(key);
-			root.color = NodeColor.Black;
-			return;
-		}
-		root = root.add(key);
-		root.color = NodeColor.Black;
-	}
-
-	public void printTree() {
-		root.LDR();
+	private enum NodeColor {
+		Black, Red
 	}
 
 	private class TreeNode {
@@ -43,17 +25,6 @@ public class RedBlackTree {
 			this.leftChild = null;
 			this.rightChild = null;
 			this.color = NodeColor.Red;
-		}
-
-		public void LDR() {
-
-			if (this.leftChild != null) {
-				this.leftChild.LDR();
-			}
-			System.out.println(this.Key);
-			if (this.rightChild != null) {
-				this.rightChild.LDR();
-			}
 		}
 
 		public TreeNode add(int key) {
@@ -154,6 +125,66 @@ public class RedBlackTree {
 			return top;
 		}
 
+		public void LDR() {
+
+			if (this.leftChild != null) {
+				this.leftChild.LDR();
+			}
+			System.out.println(this.Key);
+			if (this.rightChild != null) {
+				this.rightChild.LDR();
+			}
+		}
+
+		/**
+		 * 删除一个节点
+		 * @param key
+		 * @return
+		 */
+		public TreeNode remove(int key) {
+			TreeNode root=null;
+			if (key<this.Key&&this.leftChild!=null) {
+				this.leftChild=this.leftChild.remove(key);
+				root=this.fixAfterDelete(this.leftChild);
+			}else if(key>this.Key&&this.rightChild!=null){
+				this.rightChild=this.rightChild.remove(key);
+				root=this.fixAfterDelete(this.rightChild);
+			}else{
+				root=this.realRemove();
+			}
+			return root;
+		}
+
+		private TreeNode fixAfterDelete(TreeNode childNode) {
+			return fixAfterInsert(childNode);
+		}
+
+		private TreeNode realRemove() {
+			if (this.leftChild==null&&this.rightChild==null) {
+				//叶子节点
+				return null;
+			}else if (this.leftChild==null) {
+				//只有右节点
+				return this.rightChild;
+			}else if(this.rightChild==null){
+				//只有左边节点
+				return this.leftChild;
+			}
+			
+			TreeNode nextNode=this.findLDRNext();  //中序遍历的下一个节点 这个节点一定是个叶子节点
+			this.Key=nextNode.Key;
+			this.rightChild=this.rightChild.remove(nextNode.Key);
+			return this;
+		}
+
+		private TreeNode findLDRNext() {
+			TreeNode temp=this.rightChild;
+			while(temp!=null&&temp.leftChild!=null){
+				temp=temp.leftChild;
+			}
+			return temp;
+		}
+
 		private TreeNode rotateLL() {
 			TreeNode top=this.leftChild;
 			assert(top!=null);
@@ -171,11 +202,6 @@ public class RedBlackTree {
 		}
 
 	}
-
-	private enum NodeColor {
-		Black, Red
-	}
-
 	/**
 	 * @param args
 	 */
@@ -186,8 +212,48 @@ public class RedBlackTree {
 		redBlackTree.add(15);
 		redBlackTree.add(2);
 		redBlackTree.add(3);
+		redBlackTree.add(7);
+		redBlackTree.add(9);
+		redBlackTree.add(12);
+		redBlackTree.add(19);
 		redBlackTree.printTree();
 		System.out.println("over");
+		
+		//redBlackTree.remove(2);
+		redBlackTree.remove(15);
+		redBlackTree.printTree();
+	}
+
+	private TreeNode root;
+
+	/**
+	 * 新增一个节点
+	 * 
+	 * @param key
+	 */
+	public void add(int key) {
+		if (root == null) {
+			// 根节点
+			root = new TreeNode(key);
+			root.color = NodeColor.Black;
+			return;
+		}
+		root = root.add(key);
+		root.color = NodeColor.Black;
+	}
+
+	public void printTree() {
+		root.LDR();
+	}
+
+	/**
+	 * 删除节点
+	 * @param key
+	 */
+	public void remove(int key){
+		if (root!=null) {
+			root=root.remove(key);
+		}
 	}
 
 }
